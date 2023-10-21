0373378982c7790ee60e58ab30eec7d83cbf74df
GlitchedLime <106375444+Gl1tch3dL1m3@users.noreply.github.com>
2023-10-21 13:58:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2549,7 +2549,7 @@ async def ping(ctx):
         tttturn = int(ttt2)
 
         gameover = random.randint(6,25)
-        gamegen = 5#random.randint(3,7)
+        gamegen = random.randint(3,7)
 
         if isplaying2 == 1:
             if opponentid2 != bot.user.id:
